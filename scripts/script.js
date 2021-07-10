0ab264b1e93d94689ca37e1a397891fa6b389cd7
Rounik Ahmed <roanoarounik@gmail.com>
2021-07-10 02:23:47 -0400

    Adding Initial Files

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// Begin Scripts
+// Hi my name is Rounik welcome to my website
+console.log("Hi my name is Rounik welcome to my website");
+
+var body = document.body;
+body.children[0].children[1].style.color = 'white';
