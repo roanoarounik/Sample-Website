9e0b6ea9bf59e8361fdce36073af6c38569ada5a
rakib <rahmed@hvac.mea.com>
2025-05-23 20:59:20 -0400

    Full Scale Multipage With Simple Layout

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,8 @@ console.log("Hi my name is Rounik welcome to my website");
 
 var body = document.body;
 body.children[0].children[1].style.color = 'white';
+
+let yearElement = document.querySelector(`.yearElement`);
+if (yearElement) {
+    yearElement.innerHTML = new Date().getFullYear();
+}
